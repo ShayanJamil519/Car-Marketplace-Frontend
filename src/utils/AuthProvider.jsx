2a9e69e3c4f3a420a7e473dbae8e7d020c94da6f
ShayanJamil519 <shayanjamil500@gmail.com>
2023-05-14 09:37:40 +0500

    Page Refresh auth problem solved

--- src/utils/AuthProvider.jsx
+++ src/utils/AuthProvider.jsx
@@ -1,9 +1,11 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("isLoggedIn") === "true"
+  );
 
   const login = () => {
     localStorage.setItem("isLoggedIn", "true");
@@ -14,6 +16,12 @@ export const AuthProvider = ({ children }) => {
     localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
+
+  useEffect(() => {
+    // This effect is only run once when the component mounts
+    // and is responsible for initializing the `isLoggedIn` state.
+    setIsLoggedIn(localStorage.getItem("isLoggedIn") === "true");
+  }, []);
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
